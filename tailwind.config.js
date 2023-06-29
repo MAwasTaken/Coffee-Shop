7e127d3764ce0f3daa8d2daec18a00d8fec692e3
MAwasTaken <ce.mahdiabdollahi@gmail.com>
2023-06-29 10:19:46 +0330

    Home Section Styled

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,10 +39,15 @@ module.exports = {
 					DEFAULT: 'rem',
 					lg: '0.625rem'
 				}
+			},
+			backgroundImage: {
+				'home-mobile': 'url(../images/headerBgMobile.webp)',
+				'home-desktop': 'url(../images/headerBgDesktop.webp)'
 			}
 		}
 	},
 	screens: {
+		xs: '480px',
 		sm: '640px',
 		md: '768px',
 		lg: '1024px',
